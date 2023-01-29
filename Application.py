ec6f11534e186aff8a6e0a91cd6b820ef8d0d5bd
adrgava <adrgava@gmail.com>
2023-01-29 21:03:19 +0100

    Declare function to show the actual path

--- Application.py
+++ Application.py
@@ -1,3 +1,14 @@
 #Call of the libraries necessary for the program
 import os
 
+#Function to show the actual directory
+def show_path():
+    #Get the actual path
+    path = os.getcwd() 
+
+    #Print the path
+    print(path)   
+
+#Call the function to show the actual directory
+show_path()
+
