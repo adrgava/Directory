0fd00fe6e78068308033e9544aba2919088ff056
adrgava <adrgava@gmail.com>
2023-02-05 20:23:22 +0100

    Integrated new function to create a folder in the current directory

--- Application.py
+++ Application.py
@@ -4,3 +4,8 @@ import Functions
 #Call the function to show the actual directory
 Functions.show_elements()
 
+#Create a folder
+Functions.create_folder("test1")
+
+#Call the function to show the actual directory
+Functions.show_elements()
